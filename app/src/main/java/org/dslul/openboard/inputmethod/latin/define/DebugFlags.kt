67de8c76bf52454d40ee338ba5480a3f8e176d10
Chumian <chumian@example.com>
2026-08-24 12:50:29 +0800

    删除旧包目录

--- app/src/main/java/org/dslul/openboard/inputmethod/latin/define/DebugFlags.kt
+++
@@ -1,9 +0,0 @@
-package com.chumian.ime.define
-
-import android.content.SharedPreferences
-
-object DebugFlags {
-    const val DEBUG_ENABLED = false
-    @JvmStatic
-    fun init(prefs: SharedPreferences?) {}
-}
--- app/src/main/java/org/dslul/openboard/inputmethod/latin/define/JniLibName.kt
+++
@@ -1,5 +0,0 @@
-package com.chumian.ime.define
-
-object JniLibName {
-    const val JNI_LIB_NAME = "jni_latinime"
-}
